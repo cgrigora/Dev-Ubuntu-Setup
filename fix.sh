64152ce938aab4d941e3509fc95725f1f1f9dd31
Adelhamid Henni <abdelhamid.henni@etu-webschoolfactory.fr>
2017-11-20 14:18:19 +0100

    add fix.sh

---
+++ fix.sh
@@ -0,0 +1,15 @@
+# Get super-perm for Docker
+sudo chown "$USER":"$USER" /home/"$USER"/.docker -R
+sudo chown "$USER":"$USER" /var/run/docker.sock -R
+#Node update
+sudo nvm install --lts
+#Changing shell to Zsh
+sudo chsh -s `which zsh`
+#NVM OHMYZSH FIX
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+#Need to restart to apply all updates
+echo "You need to restart your computer to apply all changes with this command =>" 
+echo "shutdown 10sec"
+sudo shutdown -r -t 10
