10b20892487b5c5633e440d591c5d2cbf67cc75f
Adelhamid Henni <abdelhamid.henni@etu-webschoolfactory.fr>
2017-11-20 13:46:22 +0100

    update nvm

--- light-install.sh
+++ light-install.sh
@@ -20,10 +20,8 @@ sudo apt-get --assume-yes install virtualbox
 wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.6/install.sh | bash
 sudo nvm install node
 sudo nvm use node
-node -v
 
-#Node update
-nvm install --lts
+
 
 # NPM
 sudo apt-get --assume-yes install npm
@@ -74,6 +72,8 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
+#Node update
+sudo nvm install --lts
 
 # Docker Create default machine 
 sudo docker-machine create -d virtualbox fastit
